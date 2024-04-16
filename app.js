2854777c5b7a5dd5607adb75c1c6c036df7d360f
Tehminanaz <tn0457584@gmail.com>
2024-04-16 12:04:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentance to count the word:"
+    }
+]);
+const words = answer.sentence.trim().split("");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
---
+++ app.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+
+const answer : {
+
+    sentence : string
+
+} =await inquirer .prompt(
+    [
+        {
+            name : "sentence",
+            type : "input",
+            message : "Enter your sentance to count the word:"
+        }
+    ]
+)
+
+const words = answer.sentence.trim().split("")
+
+console.log (words)
+
+console.log (`Your sentence word count is ${words.length}`);
